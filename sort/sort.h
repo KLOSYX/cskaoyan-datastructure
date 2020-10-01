c9a771aed6e250c5ee6232a8f297bc208a82681f
anbinx <klosyx@outlook.com>
2020-10-01 22:25:18 +0800

    Fix sort.h

--- sort/sort.h
+++ sort/sort.h
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#define TEST(Fun) TEST_A; TEST_N; MAIN(Fun); PRINT;
 #define TEST_A int a[10] = {20, 40, 30, 10, 60, 60, 55, 90, 80, 70}
 #define TEST_N int n = 10
-#define MAIN(Fun) Fun(a, n)
+#define MAIN(F) F(a, n)
 #define PRINT for(int i = 0; i < n; ++i){printf("%d\n", a[i]);}
