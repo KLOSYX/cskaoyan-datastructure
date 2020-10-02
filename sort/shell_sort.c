a70182570914a6a427b536db703d5954422d1c11
anbinx <klosyx@outlook.com>
2020-10-02 20:10:23 +0800

    Add shell sort

---
+++ sort/shell_sort.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+
+void shellSort(int a[], int n) {
+    int i, j ,d, temp;
+    for (d = n / 2; d >= 1; d /= 2) {
+        for (i = d; i < n; ++i) {
+            if (a[i-d] > a[i]) {
+                temp = a[i];
+                for (j = i - d; a[j] > temp && j >= 0; j -= d) {
+                    a[j + d] = a[j];
+                }
+                a[j + d] = temp;
+            }
+        }
+    }
+}
+
+void main() {
+    TEST_A;
+    TEST_N;
+    MAIN(shellSort);
+    CHECK;
+}
--- sort/sort.h
+++ sort/sort.h
@@ -1,5 +1,29 @@
 #include <stdio.h>
 #define TEST_A int a[10] = {20, 40, 30, 10, 60, 60, 55, 90, 80, 70}
 #define TEST_N int n = 10
-#define MAIN(F) F(a, n)
-#define PRINT for(int i = 0; i < n; ++i){printf("%d\n", a[i]);}
+#define MAIN(F)  \
+    do           \
+    {            \
+        F(a, n); \
+    } while (0)
+#define PRINT                       \
+    do                              \
+    {                               \
+        for (int i = 0; i < n; ++i) \
+        {                           \
+            printf("%d\n", a[i]);   \
+        }                           \
+    } while (0)
+#define CHECK                                       \
+    do                                              \
+    {                                               \
+        for (int i = 1; i < n; ++i)                 \
+        {                                           \
+            if (a[i] < a[i - 1])                    \
+            {                                       \
+                printf("------TEST FAILURE------"); \
+                return;                             \
+            }                                       \
+        }                                           \
+        printf("-----TEST PASS-----");              \
+    } while (0)
